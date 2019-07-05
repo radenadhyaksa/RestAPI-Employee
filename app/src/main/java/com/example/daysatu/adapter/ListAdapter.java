d60e012d12448a8fde132a3f011619199003fdd8
raden adhyaksa <raksa69@gmail.com>
2019-07-06 00:24:03 +0700

    Add package Adapter in Java File

--- app/src/main/java/com/example/daysatu/adapter/ListAdapter.java
+++ app/src/main/java/com/example/daysatu/adapter/ListAdapter.java
@@ -1,4 +1,4 @@
-package com.example.daysatu;
+package com.example.daysatu.adapter;
 
 import android.app.Activity;
 import android.content.Context;
@@ -10,6 +10,7 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.daysatu.R;
 import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
--- app/src/main/java/com/example/daysatu/menu/EmployeeActivity.java
+++ app/src/main/java/com/example/daysatu/menu/EmployeeActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.example.daysatu.ListAdapter;
+import com.example.daysatu.adapter.ListAdapter;
 import com.example.daysatu.R;
 import com.example.daysatu.RestProcess;
 import com.loopj.android.http.AsyncHttpClient;
